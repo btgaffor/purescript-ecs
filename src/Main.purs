4188cf7d30c7c328c47d785224b7ed3038d91b47
Brandon Gafford <brandon.gafford@gmail.com>
2020-05-22 12:34:55 -0500

    working set + instance for Tuple

--- src/Main.purs
+++ src/Main.purs
@@ -2,7 +2,8 @@ module Main where
 
 import Prelude
 import Control.Monad.Reader (ReaderT, asks, runReaderT)
-import Control.Monad.State (evalStateT, gets)
+import Control.Monad.State (evalStateT, gets, lift, modify_)
+import Control.Monad.State as State
 import Data.Either (Either(..))
 import Data.Map (Map, empty, insert)
 import Data.Map as DataMap
@@ -13,20 +14,33 @@ import Effect (Effect)
 import Effect.Console (log, logShow)
 import Partial.Unsafe (unsafePartial)
 import Path (type (/), Choice, Param, S, Anything, parseUrl)
-import Storage (class Has, Entity(..), System, get, getStore, initStore)
+import Storage (class Has, class PutStore, Entity(..), System, get, getStore, initStore, set)
 import Type.Prelude (Proxy(..))
 
 data Position
   = Position Int
 
+instance showPosition :: Show Position where
+  show (Position num) = show num
+
 data Velocity
   = Velocity Int
+
+instance showVelocity :: Show Velocity where
+  show (Velocity num) = show num
 
 data World
   = World
     { positions :: Map Entity Position
     , velocities :: Map Entity Velocity
     }
+
+unWorld ::
+  World ->
+  { positions :: Map Entity Position
+  , velocities :: Map Entity Velocity
+  }
+unWorld (World world) = world
 
 initWorld :: World
 initWorld =
@@ -36,15 +50,24 @@ initWorld =
     }
 
 instance hasPosition :: Has World Position (Map Entity Position) where
-  getStore _ = gets $ \(World world) -> world.positions
+  getStore _ = gets (unWorld >>> _.positions)
+
+instance putStorePosition :: PutStore World (Map Entity Position) where
+  putStore positions = modify_ (unWorld >>> _ { positions = positions } >>> World)
 
 instance hasVelocity :: Has World Velocity (Map Entity Velocity) where
-  getStore _ = gets $ \(World world) -> world.velocities
+  getStore _ = gets (unWorld >>> _.velocities)
+
+instance putStoreVelocity :: PutStore World (Map Entity Velocity) where
+  putStore velocities = modify_ (unWorld >>> _ { velocities = velocities } >>> World)
 
 runGame :: System World Int
 runGame = do
-  Position p /\ Velocity v <- get (Entity 5)
-  pure $ p + v
+  Position p1 /\ Velocity v1 <- get (Entity 5)
+  set (Entity 5) $ (Position $ p1 + v1) /\ (Velocity $ v1 + 1)
+  Position p2 /\ Velocity v2 <- get (Entity 5)
+  lift $ logShow (p2 /\ v2)
+  pure $ p2 + v2
 
 main :: Effect Unit
 main = do
--- src/Storage.purs
+++ src/Storage.purs
@@ -3,11 +3,12 @@ module Storage where
 import Prelude
 
 import Control.Apply (lift2)
-import Control.Monad.State (StateT(..))
-import Data.Map (Map, empty, insert, lookup)
-import Data.Maybe (fromJust)
+import Control.Monad.State (StateT(..), lift)
+import Data.Map (Map, empty, insert, lookup, update)
+import Data.Maybe (Maybe(..), fromJust)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
+import Effect.Console (logShow)
 import Partial.Unsafe (unsafePartial)
 import Type.Prelude (Proxy(..))
 
@@ -18,6 +19,8 @@ derive instance eqEntity :: Eq Entity
 derive instance ordEntity :: Ord Entity
 instance showEntity :: Show Entity where
   show (Entity value) = show value
+
+-- Has
 
 class Has w c s | c -> s where
   getStore :: Proxy c -> System w s
@@ -30,11 +33,15 @@ instance hasTuple :: (Has w c1 s1, Has w c2 s2) => Has w (Tuple c1 c2) (Tuple s1
     in
     lift2 Tuple storage1 storage2
 
+-- Init
+
 class ExplInit s where
   initStore :: s
 
 instance explInitMap :: ExplInit (Map Entity c) where
   initStore = empty
+
+-- Get
 
 class ExplGet s c | s -> c where
   explGet :: Entity -> s -> c
@@ -50,3 +57,34 @@ get :: forall w c s. Has w c s => ExplGet s c => Entity -> System w c
 get entity = do
   store <- getStore (Proxy :: Proxy c)
   pure $ explGet entity store
+
+-- Set
+
+class PutStore w s where
+  putStore :: s -> System w Unit
+
+instance putTuple :: (PutStore w s1, PutStore w s2) => PutStore w (Tuple s1 s2) where
+  putStore (Tuple storage1 storage2) = do
+    putStore storage1
+    putStore storage2
+    pure unit
+
+class ExplSet s c where
+  explSet :: s -> Entity -> c -> s
+
+instance explSetMap :: ExplSet (Map Entity c) c where
+  explSet map entity component = update (\_ -> Just component) entity map
+
+instance explSetTuple :: (ExplSet s1 c1, ExplSet s2 c2) => ExplSet (Tuple s1 s2) (Tuple c1 c2) where
+  explSet (Tuple s1 s2) entity (Tuple c1 c2) =
+    let
+      newS1 = explSet s1 entity c1
+      newS2 = explSet s2 entity c2
+    in
+    Tuple newS1 newS2
+
+set :: forall w c s. Show s => Has w c s => ExplSet s c => PutStore w s => Entity -> c -> System w Unit
+set entity component = do
+  store <- getStore (Proxy :: Proxy c)
+  newStore <- pure $ explSet store entity component
+  putStore newStore
