43f9ba5a666928b2d45f8218e70766e3e6d040d3
Brandon Gafford <brandon.gafford@gmail.com>
2020-05-25 11:04:58 -0500

    newEntity working

--- src/Main.purs
+++ src/Main.purs
@@ -1,44 +1,38 @@
 module Main where
 
 import Prelude
-
-import Control.Monad.Reader (ReaderT, asks, runReaderT)
 import Control.Monad.State (evalStateT, gets, lift, modify_)
-import Control.Monad.State as State
-import Data.Either (Either(..))
-import Data.Map (Map, empty, insert)
-import Data.Map as DataMap
-import Data.Maybe (fromJust)
-import Data.Tuple (Tuple(..))
+import Data.Map (Map, insert)
+import Data.Tuple (Tuple)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Console (log, logShow)
-import Partial.Unsafe (unsafePartial)
-import Path (type (/), Choice, Param, S, Anything, parseUrl)
-import Storage (class Has, class PutStore, Entity(..), System, exists, get, getStore, initStore, members, set)
+import Storage (class Has, class PutStore, Entity(..), EntityCount, Global, System, cmap, exists, get, initStore, members, newEntity, set)
 import Type.Prelude (Proxy(..))
 
 data Position
   = Position Int
 
 instance showPosition :: Show Position where
-  show (Position num) = show num
+  show (Position num) = "(Position " <> show num <> ")"
 
 data Velocity
   = Velocity Int
 
 instance showVelocity :: Show Velocity where
-  show (Velocity num) = show num
+  show (Velocity num) = "(Velocity " <> show num <> ")"
 
 data World
   = World
-    { positions :: Map Entity Position
+    { entityCounter :: Global EntityCount
+    , positions :: Map Entity Position
     , velocities :: Map Entity Velocity
     }
 
 unWorld ::
   World ->
-  { positions :: Map Entity Position
+  { entityCounter :: Global EntityCount
+  , positions :: Map Entity Position
   , velocities :: Map Entity Velocity
   }
 unWorld (World world) = world
@@ -46,9 +40,24 @@ unWorld (World world) = world
 initWorld :: World
 initWorld =
   World
-    { positions: initStore # insert (Entity 5) (Position 10) # insert (Entity 6) (Position 6)
+    { entityCounter: initStore
+    , positions: initStore # insert (Entity 5) (Position 10) # insert (Entity 6) (Position 6)
     , velocities: initStore # insert (Entity 5) (Velocity 5)
     }
+
+initWorld2 :: World
+initWorld2 =
+  World
+    { entityCounter: initStore
+    , positions: initStore
+    , velocities: initStore
+    }
+
+instance hasEntityCounter :: Has World EntityCount (Global EntityCount) where
+  getStore _ = gets (unWorld >>> _.entityCounter)
+
+instance putStoreEntityCounter :: PutStore World (Global EntityCount) where
+  putStore entityCounter = modify_ (unWorld >>> _ { entityCounter = entityCounter } >>> World)
 
 instance hasPosition :: Has World Position (Map Entity Position) where
   getStore _ = gets (unWorld >>> _.positions)
@@ -68,28 +77,49 @@ runGame = do
   set (Entity 5) $ (Position $ p1 + v1) /\ (Velocity $ v1 + 1)
   Position p2 /\ Velocity v2 <- get (Entity 5)
   lift $ logShow (p2 /\ v2)
-
   e <- exists (Entity 5) (Proxy :: Proxy Velocity)
   lift $ log $ "e: " <> show e
-
   e2 <- exists (Entity 6) (Proxy :: Proxy Position)
   lift $ log $ "e2: " <> show e2
-
   e3 <- exists (Entity 5) (Proxy :: Proxy (Tuple Position Velocity))
   lift $ log $ "e3: " <> show e3
-
   e4 <- exists (Entity 6) (Proxy :: Proxy (Tuple Position Velocity))
   lift $ log $ "e4: " <> show e4
-
   positionMembers <- members (Proxy :: Proxy Velocity)
   lift $ logShow positionMembers
-
   bothMembers <- members (Proxy :: Proxy (Tuple Position Velocity))
   lift $ log $ "bothMembers: " <> show bothMembers
-
+  Position pBefore /\ Velocity vBefore <- get (Entity 5)
+  lift $ log $ "position 5 before: " <> show pBefore
+  lift $ log $ "velocity 5 before: " <> show vBefore
+  cmap \(Position p) -> Position (p + 1)
+  Position pAfter1 /\ Velocity vAfter1 <- get (Entity 5)
+  lift $ log $ "position 5 after1: " <> show pAfter1
+  lift $ log $ "velocity 5 after1: " <> show vAfter1
+  cmap \(Position p /\ Velocity v) -> Position (p + v) /\ Velocity (v - 1)
+  Position pAfter2 /\ Velocity vAfter2 <- get (Entity 5)
+  lift $ log $ "position 5 after2: " <> show pAfter2
+  lift $ log $ "velocity 5 after2: " <> show vAfter2
+  ety1 <- newEntity (Position 3)
+  lift $ log $ "ety1: " <> show ety1
+  ety2 <- newEntity (Position 33)
+  lift $ log $ "ety2: " <> show ety2
+  Position ety2P <- get ety2
+  lift $ log $ "ety2P: " <> show ety2P
   pure $ p2 + v2
+
+runGame2 :: System World Int
+runGame2 = do
+  entity1 <- newEntity $ Position 5 /\ Velocity 2
+  -- lift $ logShow entity1
+  -- storage <- getStore (Proxy :: Proxy Position)
+  -- lift $ logShow storage
+  cmap \(Position p /\ Velocity v) -> Position (p + v) /\ Velocity (v - 1)
+  Position p /\ Velocity v <- get entity1
+  lift $ log $ "Position: " <> show p <> ", Velocity: " <> show v
+  pure p
 
 main :: Effect Unit
 main = do
   p <- evalStateT runGame initWorld
-  log $ show $ p
+  logShow p
--- src/Storage.purs
+++ src/Storage.purs
@@ -1,24 +1,37 @@
 module Storage where
 
 import Prelude
-
 import Control.Apply (lift2)
-import Control.Monad.State (StateT(..), lift)
+import Control.Monad.State (StateT)
+import Data.Foldable (foldM)
 import Data.List (List, filter)
-import Data.Map (Map, empty, insert, lookup, update, values)
+import Data.Map (Map, empty, insert, lookup)
 import Data.Map.Internal (keys)
-import Data.Maybe (Maybe(..), fromJust, isJust)
+import Data.Maybe (fromJust, isJust)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Console (logShow)
 import Partial.Unsafe (unsafePartial)
 import Type.Prelude (Proxy(..))
 
-type System w a = StateT w Effect a
+----------
+-- Core --
+----------
+type System w a
+  = StateT w Effect a
 
-data Entity = Entity Int
+type SystemT w m a
+  = StateT w m a
+
+data Entity
+  = Entity Int
+
+data Global c
+  = Global c
+
 derive instance eqEntity :: Eq Entity
+
 derive instance ordEntity :: Ord Entity
+
 instance showEntity :: Show Entity where
   show (Entity value) = show value
 
@@ -31,26 +44,11 @@ class ExplInit s where
 class ExplGet s c | s -> c where
   explGet :: Entity -> s -> c
 
-get :: forall w c s. Has w c s => ExplGet s c => Entity -> System w c
-get entity = do
-  store <- getStore (Proxy :: Proxy c)
-  pure $ explGet entity store
-
 class ExplExists s where
   explExists :: Entity -> s -> Boolean
 
-exists :: forall w c s. Has w c s => ExplExists s => Entity -> Proxy c -> System w Boolean
-exists entity proxyC = do
-  storage <- getStore proxyC
-  pure $ explExists entity storage
-
 class ExplMembers s where
   explMembers :: s -> List Entity
-
-members :: forall w c s. Has w c s => ExplMembers s => Proxy c -> System w (List Entity)
-members proxyC = do
-  storage <- getStore proxyC
-  pure $ explMembers storage
 
 class PutStore w s where
   putStore :: s -> System w Unit
@@ -58,18 +56,93 @@ class PutStore w s where
 class ExplSet s c where
   explSet :: s -> Entity -> c -> s
 
-set :: forall w c s. Show s => Has w c s => ExplSet s c => PutStore w s => Entity -> c -> System w Unit
+-------------
+-- Systems --
+-------------
+get :: forall w c s. Has w c s => ExplGet s c => Entity -> System w c
+get entity = do
+  store <- getStore (Proxy :: Proxy c)
+  pure $ explGet entity store
+
+exists :: forall w c s. Has w c s => ExplExists s => Entity -> Proxy c -> System w Boolean
+exists entity proxyC = do
+  storage <- getStore proxyC
+  pure $ explExists entity storage
+
+members :: forall w c s. Has w c s => ExplMembers s => Proxy c -> System w (List Entity)
+members proxyC = do
+  storage <- getStore proxyC
+  pure $ explMembers storage
+
+set :: forall w c s. Has w c s => ExplSet s c => PutStore w s => Entity -> c -> System w Unit
 set entity component = do
   store <- getStore (Proxy :: Proxy c)
   newStore <- pure $ explSet store entity component
   putStore newStore
 
--- Map
+-- TODO: performance measurements on these 2 implementations
+cmap ::
+  forall w c1 c2 s1 s2.
+  Has w c1 s1 =>
+  ExplMembers s1 =>
+  ExplGet s1 c1 =>
+  Has w c2 s2 =>
+  ExplSet s2 c2 =>
+  PutStore w s2 =>
+  (c1 -> c2) -> System w Unit
+-- cmap transform = do
+--   entities <- members (Proxy :: Proxy c1)
+--   foldM
+--     ( \accumulator entity -> do
+--         old <- get entity
+--         set entity (transform old)
+--     )
+--     unit
+--     entities
+cmap f = do
+  s1 <- getStore (Proxy :: Proxy c1)
+  initialS2 <- getStore (Proxy :: Proxy c2)
+  let
+    entities = explMembers s1
+  newS2 <-
+    foldM
+      ( \store entity ->
+          let
+            oldComponent = explGet entity s1
 
+            newS2 = explSet store entity (f oldComponent)
+          in
+            pure newS2
+      )
+      initialS2
+      entities
+  putStore newS2
+
+newEntity ::
+  forall w s c.
+  Has w c s =>
+  ExplSet s c =>
+  PutStore w s =>
+  Has w EntityCount (Global EntityCount) =>
+  ExplGet (Global EntityCount) EntityCount =>
+  ExplSet (Global EntityCount) EntityCount =>
+  PutStore w (Global EntityCount) =>
+  c -> System w Entity
+newEntity component = do
+  EntityCount currentCount <- get (Entity 0)
+  storageBefore <- getStore (Proxy :: Proxy c)
+  set (Entity currentCount) component
+  storageAfter <- getStore (Proxy :: Proxy c)
+  set (Entity 0) (EntityCount $ currentCount + 1)
+  pure $ Entity currentCount
+
+---------
+-- Map --
+---------
 instance explInitMap :: ExplInit (Map Entity c) where
   initStore = empty
 
-  -- | this will crash if there is component for that entity
+-- | this will crash if there is component for that entity
 instance explGetMap :: ExplGet (Map Entity c) c where
   explGet entity map = unsafePartial $ fromJust (lookup entity map)
 
@@ -80,33 +153,52 @@ instance explMembersMap :: ExplMembers (Map Entity c) where
   explMembers map = keys map
 
 instance explSetMap :: ExplSet (Map Entity c) c where
-  explSet map entity component = update (\_ -> Just component) entity map
+  explSet map entity component = insert entity component map
 
--- Tuple
-
+-----------
+-- Tuple --
+-----------
 instance hasTuple :: (Has w c1 s1, Has w c2 s2) => Has w (Tuple c1 c2) (Tuple s1 s2) where
-  getStore _ =
-     lift2 Tuple (getStore (Proxy :: Proxy c1)) (getStore (Proxy :: Proxy c2))
+  getStore _ = lift2 Tuple (getStore (Proxy :: Proxy c1)) (getStore (Proxy :: Proxy c2))
 
 instance explGetTuple :: (ExplGet s1 c1, ExplGet s2 c2) => ExplGet (Tuple s1 s2) (Tuple c1 c2) where
-  explGet entity (Tuple storage1 storage2) =
-    Tuple (explGet entity storage1) (explGet entity storage2)
+  explGet entity (Tuple storage1 storage2) = Tuple (explGet entity storage1) (explGet entity storage2)
 
 instance explExistsTuple :: (ExplExists s1, ExplExists s2) => ExplExists (Tuple s1 s2) where
-  explExists entity (Tuple storage1 storage2) =
-    (explExists entity storage1) && (explExists entity storage2)
+  explExists entity (Tuple storage1 storage2) = (explExists entity storage1) && (explExists entity storage2)
 
 instance explMembersTuple :: (ExplMembers s1, ExplExists s2) => ExplMembers (Tuple s1 s2) where
   explMembers (Tuple storage1 storage2) =
     let
       members1 = explMembers storage1
     in
-    filter (\entity -> explExists entity storage2) members1
+      filter (\entity -> explExists entity storage2) members1
 
 instance putStoreTuple :: (PutStore w s1, PutStore w s2) => PutStore w (Tuple s1 s2) where
-  putStore (Tuple storage1 storage2) =
-    putStore storage1 *> putStore storage2
+  putStore (Tuple storage1 storage2) = putStore storage1 *> putStore storage2
 
 instance explSetTuple :: (ExplSet s1 c1, ExplSet s2 c2) => ExplSet (Tuple s1 s2) (Tuple c1 c2) where
-  explSet (Tuple s1 s2) entity (Tuple c1 c2) =
-     Tuple (explSet s1 entity c1) (explSet s2 entity c2)
+  explSet (Tuple s1 s2) entity (Tuple c1 c2) = Tuple (explSet s1 entity c1) (explSet s2 entity c2)
+
+-------------------
+-- EntityCounter --
+-------------------
+data EntityCount
+  = EntityCount Int
+
+-- instance explInitMap :: ExplInit (Map Entity c) where
+--   initStore = empty
+-- instance explInitEntityCounter :: ExplInit (Global c) where
+--   initStore = EntityCounter (EntityCount 0)
+
+instance explInitEntityCounter :: ExplInit (Global EntityCount) where
+  initStore = (Global (EntityCount 0))
+
+instance explGetEntityCounter :: ExplGet (Global c) c where
+  explGet entity (Global entityCount) = entityCount
+
+instance explExistsEntityCounter :: ExplExists (Global c) where
+  explExists entity _ = true
+
+instance explSetEntityCounter :: ExplSet (Global c) c where
+  explSet _ _ entityCount = Global entityCount
